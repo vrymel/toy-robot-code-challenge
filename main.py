80a2cbad1d2387cdda9e75d8defd20c6151f5dde
vrymel <vrymel.dev@gmail.com>
2022-12-10 10:37:57 +0800

    added robot turn action

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from enum import Enum
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌥⇧B to toggle the breakpoint.
+class Direction(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
 
 
-# Press the green button in the gutter to run the script.
+class Robot:
+    def __init__(self, facing=Direction.NORTH):
+        self.facing = facing
+
+    def turn_left(self):
+        target_direction = self.facing.value - 1
+        if target_direction < Direction.NORTH.value:
+            target_direction = Direction.WEST.value
+
+        self.facing = Direction(target_direction)
+
+    def turn_right(self):
+        target_direction = self.facing.value + 1
+        if target_direction > Direction.WEST.value:
+            target_direction = Direction.NORTH.value
+
+        self.facing = Direction(target_direction)
+
+    def report(self):
+        print(f'I\'m facing {self.facing}')
+
+
+class TableTop:
+    def __init__(self, dimensions=(5, 5)):
+        self.dimensions = dimensions
+
+
+def main(name):
+    robot = Robot()
+
+    while True:
+        foo = input('turn left or right')
+
+        if foo == 'left':
+            robot.turn_left()
+        if foo == 'right':
+            robot.turn_right()
+
+        robot.report()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main('PyCharm')
